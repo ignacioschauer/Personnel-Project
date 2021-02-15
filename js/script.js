0504ef2d5f7af86996962e4938d4cf00fa4c14b9
ignacioschauer <70091736+ignacioschauer@users.noreply.github.com>
2021-02-15 01:47:10 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,212 +1,139 @@
-// Get the modal
-
-var modal = document.getElementById("exampleModal");
-
-var modalTwo = document.getElementById("exampleModal2");
-
-var modalThree = document.getElementById("exampleModal3");
-
-var modalFour = document.getElementById("exampleModal4");
-
-// Get the button that opens the modal
-
-var easyOne = document.getElementById("dropdown01");
-
-var easyTwo = document.getElementById("addButton");
-
-var easyThree = document.getElementById("editButton");
-
-var easyFour = document.getElementById("deleteButton");
-
-// Get the <span> element that closes the modal
-
-var span = document.getElementsByClassName("close")[0];
-
-var btnTwo = document.getElementsByClassName("close")[1];
-
-var btnThree = document.getElementsByClassName("close")[2];
-
-var btnFour = document.getElementsByClassName("close")[3];
-
-// Get the <close> button that closes the modal
-
-var closeFirst = document.getElementById("closeModal");
-
-var closeSecond = document.getElementById("closeModal2");
-
-var closeThird = document.getElementById("closeModal3");
-
-var closeFourth = document.getElementById("closeModal4");
-
-// When the user clicks on the button, open the modal
-
-easyOne.onclick = function() {
-  modal.style.display = "block";
-  modalTwo.style.display = "none";
-  modalThree.style.display = "none";
-  modalFour.style.display = "none";
-}
-
-easyTwo.onclick = function() {
-  modalTwo.style.display = "block";
-  modal.style.display = "none";
-  modalThree.style.display = "none";
-  modalFour.style.display = "none";
-}
-
-easyThree.onclick = function() {
-  modalThree.style.display = "block";
-  modalTwo.style.display = "none";
-  modal.style.display = "none";
-  modalFour.style.display = "none";
-}
-
-easyFour.onclick = function() {
-  modalFour.style.display = "block";
-  modalThree.style.display = "none";
-  modalTwo.style.display = "none";
-  modal.style.display = "none";
-}
-
-// When the user clicks on <span> (x), close the modal
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-btnTwo.onclick = function() {
-  modalTwo.style.display = "none";
-}
-
-btnThree.onclick = function() {
-  modalThree.style.display = "none";
-}
-
-btnFour.onclick = function() {
-  modalFour.style.display = "none";
-}
-
-closeFirst.onclick = function() {
-  modal.style.display = "none";
-}
-
-closeSecond.onclick = function() {
-  modalTwo.style.display = "none";
-}
-
-closeThird.onclick = function() {
-  modalThree.style.display = "none";
-}
-
-closeFourth.onclick = function() {
-  modalFour.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  } else if (event.target == modalTwo) {
-    modalTwo.style.display = "none"
-  } else if (event.target == modalThree) {
-    modalThree.style.display = "none"
-  } else if (event.target == modalFour) {
-    modalFour.style.display = "none"
-  }
-}
-
-// Employee List
-
-//$.each(result.data.country, function(index) {
-
 $(document).ready(function () {
 
-  $.ajax ({
-    url: 'php/getAll.php',
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      id : 1
+  $("#addButton").click(function () { 
+    $('#exampleModal2').modal('show');
+    
+  });
+  // all data get
+  function get_all_data(){
+    $.get("php/get_data.php", 
+    {
+      get_data:"get_data"
     },
-    success: function (result) {
-  
-      console.log(result.data) 
-  
-        if (result.status.name == "ok") {
-  
-        
-        $.each(result.data, function(index) {
-                             
-          $('#employeeList').append(
-  
-            $("<tr>" +
-            "<td class='firstTd'><input type='checkbox' class='checkbox'></td>" +
-            "<td class='firstTd'>" + result.data[index].id + "</td>" +
-            "<td class='secondTd'>" + result.data[index].firstName + "</td>" +
-            "<td class='secondTd'>" + result.data[index].lastName + "</td>" +
-            "<td>" + result.data[index].jobTitle + "</td>" +
-            "<td>" + result.data[index].email + "</td>" +
-            "<td class='secondTd'>" + result.data[index].departmentId + "</td>" +
-            "</tr>"
-                
-          ));
-        });                    
-      };          
-    } 
+    function (data) {
+      $('#employeeList').html(data);
+      // showPage(1);
+    });
+
+  }
+  get_all_data();
+
+  // insert data
+
+  $('#btn_submit').click(function(){
+      $.ajax ({
+        url: 'php/get_data.php',
+        type: 'POST',
+        data: {
+          firstName: $('#firstName').val(),
+          lastName: $('#lastName').val(),
+          jobTitle: $('#jobTitle').val(),
+          email: $('#email').val(), 
+          departmentId: $('#departmentId').val(),
+        },
+        success: function (result) {
+          $('#exampleModal2').modal('hide');
+          get_all_data();
+          toastr.success("Add Data Successfully");
+        }
+      });
   });
-});
+  // Edit
 
-// Delete Row 
-
-/*var confirmDelete = document.getElementById("confirmDeletion");
-
-function deleteRow() {
-  document.querySelectorAll('#employeeList .checkbox:checked').forEach(e => {
-    e.parentNode.parentNode.remove()
+  $('#editButton').click(function () {
+    
+    var editid="0";
+    $('input.checkbox:checkbox:checked').each(function () {
+      editid = $(this).attr('id');
+    });
+    $('#exampleModal3').modal('show');
+    $('#firstName_update').val($('#firstName_'+editid).text());
+    $('#lastName_update').val($('#lastName_'+editid).text());
+    $('#jobTitle_update').val($('#jobTitle_'+editid).text());
+    $('#email_update').val($('#email_'+editid).text());
+    $('#departmentId_update').val($('#departmentId_'+editid).text());
+   
   });
-}
 
-confirmDelete.onclick = deleteRow ();*/
+  // update data
 
-$('#confirmDeletion').click(function(){
-  $('.checkbox:checked').each(function(){
-    $(this).parent('tr').remove();
+  $("#btn_update").click(function () { 
+    var editid="0";
+    $('input.checkbox:checkbox:checked').each(function () {
+      editid = $(this).attr('id');
+    });
+    $('#exampleModal3').modal('hide');
+    $.ajax ({
+      url: 'php/get_data.php',
+      type: 'GET',
+      data: {
+        update_id: editid,
+        firstName_update: $('#firstName_update').val(),
+        lastName_update: $('#lastName_update').val(),
+        jobTitle_update: $('#jobTitle_update').val(),
+        email_update: $('#email_update').val(), 
+        departmentId_update: $('#departmentId_update').val(),
+      },
+      success: function (result) {
+        $('#exampleModal3').modal('hide');
+        $('#firstName_'+editid).text($('#firstName_update').val());
+        $('#lastName_'+editid).text($('#lastName_update').val());
+        $('#jobTitle_'+editid).text($('#jobTitle_update').val());
+        $('#email_'+editid).text($('#email_update').val());
+        $('#departmentId_'+editid).text($('#departmentId_update').val());
+        toastr.success("Update Data Successfully");
+      }
+    });
   });
-});
 
+  // delete data
+  
+  $('#deleteButton').click(function(){
+    
+    swal({
+      title: "Are you sure. You want to delete this information? ",
+      // text: "Once deleted, you will not be able to recover this imaginary file!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      var count = 0;
+      if (willDelete) {
+        var delete_ids = $(".checkbox:checked").map(function() {
+        var row_id = $(this).attr('id');
+        $('#tr_'+row_id).remove();
+        count++;
+        return $(this).attr('id');
+      }).get().join(',');
+
+      $.ajax ({
+          url: 'php/get_data.php',
+          type: 'get',
+          data: {
+            delete_rows:delete_ids,
+          },
+          success: function (result) {
+            toastr.success("You've deleted "+count+" rows successfully ");
+          }
+        });
+      }
+    });
+  });
  
-/*$('#closeModal2').click(function(){
-
-  $.ajax ({
-    url: 'php/addPersonnel.php',
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      firstName: $('#firstName').val(),
-      lastName: $('#lastName').val(),
-      jobTitle: $('#jobTitle').val(),
-      email: $('#email').val(), 
-      departmentId: $('#departmentId').val(),
-    },
-    success: function (result) {
-  
-      console.log(result);
-
-      alert("add Personnel php is working")
-
-    }
+  // search 
+ 
+  $("#search").keyup(function () {
+      var value = this.value.toLowerCase().trim();
+      $("table tr").each(function (index) {
+          if (!index) return;
+          $(this).find("td").each(function () {
+              var id = $(this).text().toLowerCase().trim();
+              var not_found = (id.indexOf(value) == -1);
+              $(this).closest('tr').toggle(!not_found);
+              return not_found;
+      });
+    });
   });
-})*/
-
-$('#closeModal2').click(function(){
-
-      function clearInput() {
-      $("#myForm :input").each ( function() {
-        $(this).val('');
-      })
-      };
-
-      clearInput();
 });
-  
+
