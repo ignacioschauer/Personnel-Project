c646b52a49253266bb1e188c6a930b4169dac643
ignacioschauer <70091736+ignacioschauer@users.noreply.github.com>
2021-02-22 04:44:01 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
         success: function (result) {
           $('#exampleModal2').modal('hide');
           get_all_data();
-          toastr.success("Add Data Successfully");
+          toastr.success("New record created successfully");
         }
       });
   });
@@ -81,7 +81,7 @@ $(document).ready(function () {
         $('#jobTitle_'+editid).text($('#jobTitle_update').val());
         $('#email_'+editid).text($('#email_update').val());
         $('#departmentId_'+editid).text($('#departmentId_update').val());
-        toastr.success("Update Data Successfully");
+        toastr.success("Data updated successfully");
       }
     });
   });
@@ -91,8 +91,7 @@ $(document).ready(function () {
   $('#deleteButton').click(function(){
     
     swal({
-      title: "Are you sure. You want to delete this information? ",
-      // text: "Once deleted, you will not be able to recover this imaginary file!",
+      title: "Are you sure? Do you want to delete this information? ",
       icon: "warning",
       buttons: true,
       dangerMode: true,
@@ -132,8 +131,119 @@ $(document).ready(function () {
               var not_found = (id.indexOf(value) == -1);
               $(this).closest('tr').toggle(!not_found);
               return not_found;
+          });
       });
-    });
   });
+
+  // sort by table heading
+
+  $('th').click(function(){
+      var table = $(this).parents('table').eq(0)
+      console.log(table);
+      var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+      this.asc = !this.asc
+      if (!this.asc){
+        rows = rows.reverse()
+      }
+      for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+  })
+  function comparer(index) {
+      return function(a, b) {
+          var valA = getCellValue(a, index), valB = getCellValue(b, index)
+          return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+      }
+  }
+  function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
+
+  // sort dropdown
+
+  function sortByNumber(rows, selector, ascending) {
+      rows.sort(function(a, b) {
+        var numberA = parseInt($(selector, a).text(), 10);
+        var numberB = parseInt($(selector, b).text(), 10);
+        if (ascending)
+          return numberA - numberB;
+        else
+          return numberB - numberA;
+      });
+      
+      return rows;
+    }
+    
+    function sortByText(rows, selector, ascending) {
+      rows.sort(function(a, b) {
+        var textA = $(selector, a).text();
+        var textB = $(selector, b).text();
+        if (ascending)
+          return textA.localeCompare(textB);
+        else
+          return textB.localeCompare(textA);
+      });
+      
+      return rows;
+    }
+
+    function sortAllBy(field,sort) {
+      var rows = $('table tbody tr').toArray();
+      var sort_a_d = sort;
+      switch (field) {
+        case 'ID':
+        rows = sortByNumber(rows, 'td.col-id', sort_a_d);
+        break;
+       
+        case 'First Name':
+        rows = sortByText(rows, 'td.col-firstName', sort_a_d);
+        break;
+
+        case 'Last Name':
+        rows = sortByText(rows, 'td.col-lastName', sort_a_d);
+        break;
+
+        case 'Job Title':
+        rows = sortByText(rows, 'td.col-jobTitle', sort_a_d);
+        break;
+
+        case 'Email Address':
+        rows = sortByText(rows, 'td.col-email', sort_a_d);
+        break;
+
+        case 'Department ID':
+          rows = sortByText(rows, 'td.col-departmentId', sort_a_d);
+          break;
+        default:
+          console.error('Undefined sort field ' + field);
+          break;
+      }
+      
+      for (var i = 0; i < rows.length; i++) {
+        $('table tbody').append(rows[i]);
+      }
+    }
+    
+    $('#sort_by').on('change', function() {
+      var sort = $('#sort_order :selected').val();
+      if(sort == 'Ascending')
+        sortAllBy(this.value,true);
+      else  
+        sortAllBy(this.value,false);
+    });
+
+    $('#sort_order').on('change', function() {
+      var order = $('#sort_by :selected').val();
+      if(this.value == 'Ascending')
+        sortAllBy(order,true);
+      else  
+        sortAllBy(order,false);
+    });
 });
 
+function check_box(){
+  var count = $('input:checkbox:checked').length;
+    if(count == 1){
+      $("#editButton").attr("disabled", false);
+    }else{
+      $("#editButton").attr("disabled", true);
+    }
+}
+   
